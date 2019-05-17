db4f767bf1db749a35c837d03c131accacab174d
minyitan <1913343670@qq.com>
2019-05-17 23:34:22 +0800

    新田

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SuperTeamApp.Models;
+
+namespace SuperTeamApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+      public DbSet<Foodprice> Foodprices { get; set; }
+       // public DbSet<Happy> Happies { get; set; }
+    }
+}
---
+++ Data/Migrations/20190517125136_AddSomethingToModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SuperTeamApp.Data.Migrations
+{
+    public partial class AddSomethingToModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FPrice",
+                table: "Foodprices",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FPrice",
+                table: "Foodprices");
+        }
+    }
+}
