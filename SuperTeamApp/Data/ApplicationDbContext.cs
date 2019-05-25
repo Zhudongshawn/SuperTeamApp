0d37228ec38b85fd284eea9df4b122aaacb9bf40
minyitan <1913343670@qq.com>
2019-05-25 11:35:22 +0800

    update the Sprit#1

---
+++ SuperTeamApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SuperTeamApp.Models;
+
+namespace SuperTeamApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Foodprice> Foodprices { get; set; }
+
+    }
+
+}
---
+++ SuperTeamApp/Data/Migrations/20190525032307_AddSomethingToModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SuperTeamApp.Data.Migrations
+{
+    public partial class AddSomethingToModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Foodprices",
+                columns: table => new
+                {
+                    FoodId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FoodName = table.Column<string>(nullable: true),
+                    FPrice = table.Column<string>(nullable: true),
+                    PictureUrl = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Foodprices", x => x.FoodId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Foodprices");
+        }
+    }
+}
